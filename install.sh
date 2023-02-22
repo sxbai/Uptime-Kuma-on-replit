af3c470ba48f4da5cff04f925e1eee28d720d1c8
舒夏 <55978160+sxbai@users.noreply.github.com>
2023-02-22 14:12:52 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,9 +10,9 @@
 echo "准备在您的 Replit 中安装 uptime-kuma"
 echo "您是否确保在运行此脚本之前配置了 replit.nix和.replit文件？"
 git clone https://github.com/sxbai/Uptime-Kuma-on-replit.git
-mv Uptime-Kuma-on-replit/* .
 cp -r Uptime-Kuma-on-replit/.replit .replit
 cp -r Uptime-Kuma-on-replit/replit.nix replit.nix
+mv Uptime-Kuma-on-replit/* .
 rm -rf Uptime-Kuma-on-replit/
 git clone https://github.com/louislam/uptime-kuma.git
 npm install npm -g && cd uptime-kuma
