86292f4196d129f24ca741cf01a4c307bcc2e347
舒夏 <55978160+sxbai@users.noreply.github.com>
2023-05-06 16:57:22 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,8 @@
 echo "准备在您的 Replit 中安装 Uptime-Kuma"
-nix-shell -p wget
 mkdir build
 cd build & wget https://raw.githubusercontent.com/sxbai/Uptime-Kuma-on-replit/master/.replit
 cd ..
 wget -O master.zip https://codeload.github.com/louislam/uptime-kuma/zip/refs/heads/master
-ls
-nix-shell -p unzip
 unzip master.zip
 npm install npm -g
 cd uptime-kuma-master && npm run setup
